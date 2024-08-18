b1ac398ce023547374448d18bf1212940dae16aa
hsiao-msi <m10459018@gmail.com>
2024-08-18 13:27:58 +0900

    prototype of todolist

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <html>
-      <head>
-      </head>
-    
-      <body>
-        <div id="app">First React</div>
-      </body>
-    </html>
-  );
+// function App() {
+
+//     const styles = {
+//         doneDoto: {
+//         },
+//     };
+//     text-decoration-line: line-through
+// }
+
+// export default App;
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "TodoList",
+            todos: [],
+        };
+    }
+    render() {
+
+
+        return (
+            <html>
+                <head>
+                </head>
+            
+                <body>
+                    <div id="app">{this.state.title}</div>
+                    <div id="todolist">
+                        <div>
+                            <button>Drag block</button>
+                            <input type="checkbox"></input>
+                            <label>todo 1</label>
+                            <button>edit</button>
+                            <button>del</button>
+                        </div>
+                        <div>
+                            <button>Drag block</button>
+                            <input type="checkbox" checked></input>
+                            <label style={{textDecoration: 'line-through'}}>
+                                todo 2
+                            </label>
+                            <button>edit</button>
+                            <button>del</button>
+                        </div>
+                        <div>
+                            <input></input>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </body>
+            </html>
+        );
+    }
 }
-
-export default App;
