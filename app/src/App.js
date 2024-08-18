7481f4fb2864043bfcf1d15d992a39b491ad3040
hsiao-msi <m10459018@gmail.com>
2024-08-18 12:24:45 +0900

    build react

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+      </head>
+    
+      <body>
+        <div id="app">First React</div>
+      </body>
+    </html>
+  );
+}
+
+export default App;
