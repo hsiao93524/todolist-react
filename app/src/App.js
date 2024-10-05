b34db6a84dbb7941e8cacddb66959bcc615583ec
hsiao-msi <m10459018@gmail.com>
2024-10-05 23:38:10 +0900

    add flex to expand middle component

--- app/src/App.js
+++ app/src/App.js
@@ -2,29 +2,69 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 
-// function TodoItem(props) {
-//     const [checked, setChecked] = React.useState(false);
-//     const [itemTitle, setItemTitle] = React.useState(false);
-//     const itemStyle = {textDecoration: checked ? 'line-through' : 'none'};
-//     const trigger = () => setChecked((state) => !state);
+function TodoItem(props) {
+    // const [checked, setChecked] = React.useState(false);
+    // const [itemTitle, setItemTitle] = React.useState(false);
+    const itemStyle = {textDecoration: props.checked ? 'line-through' : 'none'};
+    const editStyle = {visibility: props.edit ? 'visible' : 'hidden'};
+    const staticStyle = {visibility: props.edit ? 'hidden' : 'visible'};
 
-//     checked = props.checked;
-//     itemTitle = props.title;
+    {
+        /***
+         * Q: can i write css here for temp test?
+         * Ref: https://styled-components.com/
+         */
+    }
 
-//     return (
-//         <div>
-//             <button>Drag block</button>
-//             <input 
-//                 onChange={trigger}
-//                 value={checked}
-//                 type="checkbox">
-//             </input>
-//             <label style={itemStyle}>{itemTitle}</label>
-//             <button>edit</button>
-//             <button>del</button>
-//         </div>
-//     )
-// }
+    // https://tailwindcss.com/docs/display
+
+    
+    // const editStyle = {visibility: props.edit ? 'visible' : 'hidden'};
+    // const editStyle = {display: props.edit ? 'inline' : 'none'};
+    // const staticStyle = {visibility: props.edit ? 'hidden' : 'visible'};
+    // const staticStyle = {display: props.edit ? 'none' : 'inline'};
+
+    // Todo: use index
+    // const trigger = () => setChecked((state) => !state);
+    const trigger = () => {};
+
+    console.log(props);
+
+    // checked = props.checked;
+    // itemTitle = props.title;
+
+    return (
+        <div style={{ display: 'flex' }}>
+            {
+                /***
+                 * Q: Check and operator
+                 */
+            }
+            <button style={Boolean(props.edit) ? {visibility: "hidden"} : {}}>Drag block</button>
+            <input  style={Boolean(props.edit) ? {visibility: "hidden"} : {}}
+                onChange={trigger}
+                value={props.checked}
+                type="checkbox">
+            </input>
+            {/* <label style={itemStyle}>{props.title}</label> */}
+            {
+                Boolean(props.edit) ? 
+                <input 
+                    type="text"
+                    value={props.title}
+                    style={{ flex: '1' }}
+                    // Todo: edit evnet
+                    // Ref: https://medium.com/itsoktomakemistakes/%E6%89%8B%E6%8A%8A%E6%89%8B%E6%95%99%E4%BD%A0%E4%BD%BF%E7%94%A8-react-%E5%AF%AB%E5%87%BA%E5%B8%B8%E8%A6%8B%E7%9A%84-input-%E5%85%83%E4%BB%B6-3a0326aa4fb6
+                >
+                </input> : 
+                <label style={{ ...itemStyle, flex: '1' }}>{props.title}</label>
+            }
+            {Boolean(props.edit) && <button>+</button>}
+            {Boolean(!props.edit) && <button>edit</button>}
+            {Boolean(!props.edit) && <button>del</button>}
+        </div>
+    )
+}
 
 function App() {
 
@@ -33,8 +73,9 @@ function App() {
 
     // Todo: How to use setTodos to get data from database or file
     const todos = [
-        { title: "auto todo 1", checked: true},
-        { title: "auto todo 2", checked: false},
+        { title: "auto todo 1", checked: true, edit: false},
+        { title: "auto todo 2", checked: false, edit: false},
+        { title: "auto todo 3 to edit", checked: false, edit: true},
     ]
     const [checked, setChecked] = React.useState(true);
 
@@ -51,29 +92,40 @@ function App() {
         
             <body>
                 <div id="app">{title}</div>
-                <div id="todolist">
+                <div id="todolist" style={{width: "400px"}}>
                     {
                         todos.map(
                             (todo) => (
-                                // return <TodoItem props={{title: todo.title, checked: todo.checked}} />;
+                                <TodoItem title={todo.title} checked={todo.checked} edit={todo.edit}/>
                                 
-                                <div>
-                                    <button>Drag block</button>
-                                    <input 
-                                        onChange={() => setChecked((state) => !state)}
-                                        value={todo.checked}
-                                        type="checkbox">
-                                    </input>
-                                    <label style={{textDecoration: checked ? 'line-through' : 'none'}}>{todo.title}</label>
-                                    <button>edit</button>
-                                    <button>del</button>
-                                </div>
+                                /*
+                                visibilty: https://segmentfault.com/a/1190000015116392
+                                */
+                                // Layout:
+                                // expand
+                                // flex
+
+                                // <div>
+                                //     {/* fix width */}
+                                //     <button>Drag block</button>
+                                //     {/* fix width */}
+                                //     <input 
+                                //         onChange={() => setChecked((state) => !state)}
+                                //         value={todo.checked}
+                                //         type="checkbox">
+                                //     </input>
+                                //     {/* expand width */}
+                                //     <label style={{textDecoration: checked ? 'line-through' : 'none'}}>{todo.title}</label>
+                                //     {/* fix width */}
+                                //     <button>edit</button>
+                                //     {/* fix width */}
+                                //     <button>del</button>
+                                // </div>
 
                             )
                         )
                     }
-                    {/* Coponent props*/}
-                    <div>
+                    {/* <div>
                         <button>Drag block</button>
                         <input 
                             onChange={() => setChecked((state) => !state)}
@@ -84,7 +136,6 @@ function App() {
                         <button>edit</button>
                         <button>del</button>
                     </div>
-                    {/* Coponent props*/}
                     <div>
                         <button>Drag block</button>
                         <input 
@@ -97,12 +148,22 @@ function App() {
                         </label>
                         <button>edit</button>
                         <button>del</button>
-                    </div>
+                    </div> */}
                     <div>
+                        {/* need to fix */}
+                        <button style={{visibility: "hidden"}}>Drag block</button>
+                        <input 
+                            style={{visibility: "hidden"}}
+                            type="checkbox">
+                        </input>
+                        {/* need to fix */}
+                        {/* <button style={{visibility: hidden}}>Drag block</button> */}
                         <input></input>
                         <button>+</button>
                     </div>
                 </div>
+                
+                {/* <TodoItem /> */}
             </body>
         </html>
     );
